67769407d658289aebbc598b439c308f2cd563c3
alvarowolfx <alvarowolfx@gmail.com>
2018-11-23 19:19:58 -0300

    [chore] Format firebase code

--- firebase/src/main.cpp
+++ firebase/src/main.cpp
@@ -17,17 +17,16 @@ DHT dht;
 #define WIFI_SSID "SSID"
 #define WIFI_PASSWORD "PASS"
 
-#define LED D2
-#define LEDPath "ledStatus"
-#define PublishPath "publishNewState"
-#define TemperaturePath "temperature"
-#define HumidityPath "humidity"
+#define led1_pin D2
+#define led1_path "led1"
+#define publish_path "publish"
+#define temperature_path "temperature"
+#define humidity_path "humidity"
 
 void setup()
 {
     dht.setup(D5); // data pin D5
    
-
     // Iniciar Monitor Serial
     Serial.begin(115200);
     delay(200);
@@ -66,10 +65,10 @@ void setup()
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
     
     // Setup 
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, HIGH);
-    Firebase.setInt(LEDPath, LOW);
-    Firebase.setInt(PublishPath, true);
+    pinMode(led1_pin, OUTPUT);
+    digitalWrite(led1_pin, HIGH);
+    Firebase.setInt(led1_path, LOW);
+    Firebase.setBool(publish_path, true);
 
     Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F) ");
 }
@@ -77,50 +76,50 @@ void setup()
 void loop()
 {
     // DHT
-  if(Firebase.getInt(PublishPath)){
+  if(Firebase.getBool(publish_path)){
     // Obtem os dados do sensor DHT 
     float humidity = dht.getHumidity();
     float temperature = dht.getTemperature();
-
-    
-     Serial.print(dht.getStatusString());
-     Serial.print("\t");
-     Serial.print(humidity, 1);
-     Serial.print("\t\t");
-     Serial.print(temperature, 1);
-     Serial.print("\t\t");
-     Serial.print(dht.toFahrenheit(temperature), 1);
-     Serial.print("\n");
+      
+    Serial.print(dht.getStatusString());
+    Serial.print("\t");
+    Serial.print(humidity, 1);
+    Serial.print("\t\t");
+    Serial.print(temperature, 1);
+    Serial.print("\t\t");
+    Serial.print(dht.toFahrenheit(temperature), 1);
+    Serial.print("\n");
 
  
     if(!isnan(humidity) && !isnan(temperature)){
       // Manda para o firebase
-      Firebase.pushFloat(TemperaturePath, temperature);
-      Firebase.pushFloat(HumidityPath, humidity);    
+      Firebase.pushFloat(temperature_path, temperature);
+      Firebase.pushFloat(humidity_path, humidity);    
       //publishNewState = false;
     }else{
       Serial.println("Error Publishing");
     }
+
   }
 
-    if (Firebase.getInt(LEDPath))
-    {
-        digitalWrite(LED, LOW);
-    }
+  if (Firebase.getInt(led1_path))
+  {
+      digitalWrite(led1_pin, LOW);
+  }
 
-    else
-    {
-        digitalWrite(LED, HIGH);
-    }
+  else
+  {
+      digitalWrite(led1_pin, HIGH);
+  }
 
-    if (Firebase.failed()) // Check for errors 
-    {
-        Serial.print("setting /number failed:");
-        Serial.println(Firebase.error());
-        return;
-    }
-    delay(1000);
+  if (Firebase.failed()) // Check for errors 
+  {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());
+      return;
+  }
 
+  delay(1000);
 }
 
 
